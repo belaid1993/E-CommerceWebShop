bc30e339329b486671cdb52786440dd13ff5413d
belaid reda <abdelkadirbelaid@gmail.com>
2021-01-09 11:28:12 +0000

    creation ecommerce store and add to cart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 //import Products from './components/Products/Products';
 //import Navbar from './components/Navbar/Navbar';
 
-import {Products, Navbar } from './components';
+import { Products, Navbar } from './components';
 
 const App = () => {
+
+    const [products, setProducts] = useState([])
+    const [cart, setCart] = useState({})
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list()
+        setProducts(data)
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity)
+        setCart(item.cart)
+    }
+
+    console.log(cart)
+
     return (
         <div>
-            <Navbar />
-            <Products />
+            <Navbar totalItems={cart.total_items} />
+            <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,16 +3,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, nom: 'a', description: 'blalalala', price: '$12', image: 'https://www.leparisien.fr/resizer/P_fjHpR5pTAuf_ZJud3Bl1PSz-w=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/LFTEZ6YS3ERLJKGKXPIZKW3H4I.jpg' },
-    { id: 2, nom: 'b', description: 'blalalala', price: '$12', image: 'https://www.leparisien.fr/resizer/P_fjHpR5pTAuf_ZJud3Bl1PSz-w=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/LFTEZ6YS3ERLJKGKXPIZKW3H4I.jpg' },
-    { id: 3, nom: 'c', description: 'blalalala', price: '$12', image: 'https://www.leparisien.fr/resizer/P_fjHpR5pTAuf_ZJud3Bl1PSz-w=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/LFTEZ6YS3ERLJKGKXPIZKW3H4I.jpg' },
-    { id: 4, nom: 'a', description: 'blalalala', price: '$12', image: 'https://www.leparisien.fr/resizer/P_fjHpR5pTAuf_ZJud3Bl1PSz-w=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/LFTEZ6YS3ERLJKGKXPIZKW3H4I.jpg' },
-    { id: 5, nom: 'b', description: 'blalalala', price: '$12', image: 'https://www.leparisien.fr/resizer/P_fjHpR5pTAuf_ZJud3Bl1PSz-w=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/LFTEZ6YS3ERLJKGKXPIZKW3H4I.jpg' },
-    { id: 6, nom: 'c', description: 'blalalala', price: '$12', image: 'https://www.leparisien.fr/resizer/P_fjHpR5pTAuf_ZJud3Bl1PSz-w=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/LFTEZ6YS3ERLJKGKXPIZKW3H4I.jpg' }
-];
 
-const Products = () => {
+const Products = ({ products , onAddToCart }) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
@@ -20,7 +12,7 @@ const Products = () => {
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3} >
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
